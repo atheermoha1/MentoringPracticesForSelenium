7ee791cf2937780486a23d7616e7c1b9edab6524
Atheer <atheermoha17@gmail.com>
2025-09-12 18:35:48 +0300

    practices

--- src/test/java/assignments/day06/T02_Action.java
+++ src/test/java/assignments/day06/T02_Action.java
@@ -1,4 +1,72 @@
 package assignments.day06;
 
-public class T02_Action {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+
+import java.util.List;
+
+public class T02_Action extends TestBase {
+    /*
+Go to https://claruswaysda.github.io/submit-button.html
+Click on submit
+Verify link contains 'actionsClickDrag'
+Drag 'Drag me' to 'Drop here'
+Right-click on 'Right-click' me
+Double-click on 'Double-click me'
+Hover on 'Hover over me'
+Verify all actions' success messages.
+*/
+String url = "https://claruswaysda.github.io/submit-button.html";
+    @Test
+    void test02() throws InterruptedException {
+
+        driver.get(url);
+        By submitButton = By.id("submitButton");
+        driver.findElement(submitButton).click();
+        //switch to new tab
+        String handle1= driver.getWindowHandle();
+        for (String h1: driver.getWindowHandles()){
+            if(!h1.equals(handle1)){
+                driver.switchTo().window(h1);
+                String currentUrl = driver.getCurrentUrl();
+                Assertions.assertTrue(currentUrl.contains("actionsClickDrag"));
+                break;
+            }
+        }
+        // Drag and Drop
+        WebElement dragMe = driver.findElement(By.id("drag1"));
+        WebElement dropHere = driver.findElement(By.id("drop1"));
+        Actions actions = new Actions(driver);
+        actions.dragAndDrop(dragMe, dropHere).perform();
+        Assertions.assertTrue(driver.findElement(By.id("dragSuccessMessage")).isDisplayed());
+
+        // Right Click
+        WebElement rightClickMe = driver.findElement(By.id("showSuccessButton"));
+        actions.contextClick(rightClickMe).perform();
+        Assertions.assertTrue(driver.findElement(By.id("rightClickSuccessMessage")).isDisplayed());
+
+
+        // Double Click
+        WebElement doubleClickMe = driver.findElement(By.id("doubleClickButton"));
+        actions.doubleClick(doubleClickMe).perform();
+        Assertions.assertTrue(driver.findElement(By.id("doubleClickSuccessMessage")).isDisplayed());
+
+        // Hover Over
+        WebElement hoverOverMe = driver.findElement(By.id("hoverButton"));
+        actions.moveToElement(hoverOverMe).perform();
+        Assertions.assertTrue(driver.findElement(By.id("hoverSuccessMessage")).isDisplayed());
+
+
+//        // Verify success messages
+//        List<WebElement> successMessages = driver.findElements(By.className("message"));
+//        for (WebElement message : successMessages) {
+//            System.out.println("messages = " + message.getText());
+//            Assertions.assertTrue(message.isDisplayed());
+//
+//        }
+    }
 }
